ee26e193538d0cd3352a3f1980c179343dd107aa
Maya Najarian <mnajarian@csbio-desktop001.csbio.unc.edu>
2017-04-12 14:01:04 -0400

    Sorting scheme that doesn't choke on large integers

--- suffixSort.py
+++ suffixSort.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-
 import csv
 import argparse
 import itertools
@@ -11,6 +9,23 @@ def bwtOrder(kmer):
     for base in reversed(kmer):
         value = (value*4) + basemap[base]
     return value
+
+def bwtsort(atup, btup):
+    aseq = atup[0]
+    bseq = btup[0]
+    for i in xrange(len(aseq)):
+        abase = aseq[-1-i]
+        bbase = bseq[-1-i]
+        if (abase > bbase):
+            return 1
+        elif (abase < bbase):
+            return -1
+    else:
+        if (atup[1] > btup[1]):
+            return 1
+        elif (atup[1] < btup[1]):
+            return -1
+    return 0
 
 def gatherProbes(pf):
     probes = []
@@ -27,17 +42,27 @@ def generateProbeFiles(pf, oDir):
     for kmer in sixmers:
 	probeList = []
 	for p in probes:
-	    if p[-6:]==s:
+	    if p[0][-6:]==kmer:
 		probeList.append(p)
-	probeInt = numpy.empty((len(probeList),), dtype='uint64')
-	for i, (seq, vid, ptype) in enumerate(probeList):
-    	    probeInt[i] = bwtOrder(seq)
-	order = numpy.argsort(probeInt, kind='heapsort')
-	fp = open(oDir+"/%s.csv" % kmer, 'w')
-	fp.write("probeseq,vid,ptype\n")
-	for i in order:
-    	    fp.write("%s,%d,%s\n" % probeList[i])
-	fp.close()
+        probeList = sorted(probeList, cmp=bwtsort)
+        fp = open(oDir+"/%s.csv" % kmer, 'w')
+        fp.write("probeseq,vid,ptype\n")
+        for i in xrange(len(probeList)):
+            fp.write("%s,%s,%s\n" % probeList[i])
+        fp.close()
+
+        #print len(probeList)
+        #probeInt = numpy.empty((len(probeList),), dtype='uint64')
+        #for i, (seq, vid, ptype) in enumerate(probeList):
+        #    print i
+        #    print bwtOrder(seq)
+        #    probeInt[i] = bwtOrder(seq)
+        #order = numpy.argsort(probeInt, kind='heapsort')
+        #fp = open(oDir+"/%s.csv" % kmer, 'w')
+        #fp.write("probeseq,vid,ptype\n")
+        #for i in order:
+        #    fp.write("%s,%d,%s\n" % probeList[i])
+        #fp.close()
 
 
 if __name__=='__main__':
