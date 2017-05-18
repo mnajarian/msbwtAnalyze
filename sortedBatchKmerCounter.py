77088653fa7857d7086788f0112162553f802ddb
Maya Najarian <mnajarian@csbio-desktop001.csbio.unc.edu>
2017-05-18 14:12:49 -0400

    Add sortedBatchKmerCounter -- sorts a probe file before running

--- sortedBatchKmerCounter.py
+++ sortedBatchKmerCounter.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-## --- msbwtAnalyze/batchKmerCounter.py --- ##
+## --- msbwtAnalyze/sortedBatchKmerCounter.py --- ##
 ##      Date: 18 May 2017
 ##      Purpose: given single probefile, query an msbwt for each kmer and record the resulting counts
 
@@ -16,7 +16,24 @@ import argparse
 import socket
 import itertools
 
-def gatherProbes (pf):
+def bwtsort(atup, btup):
+    aseq = atup[0]
+    bseq = btup[0]
+    for i in xrange(len(aseq)):
+        abase = aseq[-1-i]
+        bbase = bseq[-1-i]
+        if (abase > bbase):
+            return 1
+        elif (abase < bbase):
+            return -1
+    else:
+        if (atup[1] > btup[1]):
+            return 1
+        elif (atup[1] < btup[1]):
+            return -1
+    return 0
+
+def gatherProbes(pf):
     probes = []
     with open(pf, 'rb') as f:
         reader = csv.reader(f)
@@ -30,9 +47,10 @@ def generate_counts(bwtfile, probe_file, out_file):
     print 'Started: {}'.format(start)
     msbwt = MultiStringBWT.loadBWT(bwtfile)
     probes = gatherProbes(probe_file)
+    probesSorted = sorted(probes, cmp=bwtsort)
     counts = []
     counter = 0
-    for row in probes:
+    for row in probesSorted:
         counts.append(msbwt.countOccurrencesOfSeq(row[0]))
         counter += 1
         if (counter & 1023) == 0:
@@ -41,8 +59,8 @@ def generate_counts(bwtfile, probe_file, out_file):
     print 'Finish: {}'.format(finish-start)
     with open(out_file,'w') as outfile:
         wr = csv.writer(outfile)
-        wr.writerow(['probeseq', 'vid', 'ptype', 'count'])
-        wr.writerows([x+(y,) for x,y in itertools.izip(probes, counts)])
+        wr.writerow(['probeseq', 'vid', 'ptype','counts'])
+        wr.writerows([x+(y,) for x,y in itertools.izip(probesSorted, counts)])
     finish = time.time()
     print 'Wrote file: {}'.format(finish-start)
 
